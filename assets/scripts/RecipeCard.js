0be89b008db3b8a224134dff189e4d895009b73f
Will <59666193+taoboliao@users.noreply.github.com>
2021-10-31 22:58:41 -0700

    Update RecipeCard.js

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -234,7 +234,7 @@ function getUrl(data) {
  * @returns {String} If found, it retuns the name of the org as a string, otherwise null
  */
 function getOrganization(data) {
-  if (data.publisher.name) return data.publisher.name;
+  if (data.publisher?.name) return data.publisher?.name;
   if (data['@graph']) {
     for (let i = 0; i < data['@graph'].length; i++) {
       if (data['@graph'][i]['@type'] == 'Organization') {
